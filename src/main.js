79ca335cbff87a030ce28dddaf3bed798c45d987
c-e-l-i-a <celia_torno@yahoo.com>
2023-01-28 19:43:14 +0100

    home layout + nav bar

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const menuBtn = document.getElementById('icon-list')
+
+/*SCROLL FUNCTIONS*/
+
+function disableScroll() {
+    document.body.classList.add("stop-scrolling");
+}
+
+function enableScroll() {
+    document.body.classList.remove("stop-scrolling");
+}
+
+/*ACTIVE NAVBAR*/
+ 
+menuBtn.onclick = function() {
+   const navBar = document.querySelector('.nav-bar')
+    navBar.classList.toggle('active')
+
+    if(navBar.classList.contains('active')){
+        disableScroll()
+    } else {
+        enableScroll();
+    }
+}
+
