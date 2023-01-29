2b04c4615696d618ef7f8d7f651d4b7b968919f1
c-e-l-i-a <celia_torno@yahoo.com>
2023-01-29 16:16:51 +0100

    cart page, img reponsive

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const menuBtn = document.getElementById('icon-list')
+const cartBtn = document.getElementById('icon-cart')
 
-/*SCROLL FUNCTIONS*/
+/* SCROLL FUNCTIONS */
 
 function disableScroll() {
     document.body.classList.add("stop-scrolling");
@@ -10,13 +11,26 @@ function enableScroll() {
     document.body.classList.remove("stop-scrolling");
 }
 
-/*ACTIVE NAVBAR*/
+/* ACTIVE NAVBAR */
  
 menuBtn.onclick = function() {
    const navBar = document.querySelector('.nav-bar')
     navBar.classList.toggle('active')
 
     if(navBar.classList.contains('active')){
+        disableScroll()
+    } else {
+        enableScroll();
+    }
+}
+
+/* ACTIVE CART PAGE */
+
+cartBtn.onclick = function() {
+    const cartPage = document.querySelector('.cart-container')
+    cartPage.classList.toggle('active')
+
+    if(cartPage.classList.contains('active')) {
         disableScroll()
     } else {
         enableScroll();
